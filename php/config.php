548a74ad6f8016fdc0080756a5bdc108e3bbc791
nandin <>
2024-03-10 11:24:07 +0800

    5-r dasgal 13-r minute hurtel code-iig hiisen

---
+++ php/config.php
@@ -0,0 +1,14 @@
+<?php
+
+$hostname = "localhost"; // Corrected the typo here
+$username = "root";
+$password = "";
+$db_name = "phpchatapp_db";
+
+$conn = mysqli_connect($hostname, $username, $password, $db_name);
+
+if (!$conn) {
+    echo 'Database connection error' . mysqli_connect_error();
+}
+
+?>
---
+++ users.php
@@ -0,0 +1,47 @@
+<?php 
+
+    include_once "header.php";
+
+?>
+
+    <body>
+      
+        <div class="wrapper">
+            <section class="users">
+
+                <header>
+
+                    <div class="content">
+
+                        <img src="php/images/1709890279cutie 10 @@.jpg" alt="">
+
+                        <div class="details">
+
+                            <span>First Last</span>
+                            <p>Online</p>
+
+                        </div>
+
+                    </div>
+
+                    <a href="php/logout.php" class="logout">Logout</a>
+
+                </header>
+
+                <div class="search">
+
+                    <span class="text">Select an user to start chat</span>
+                    <input type="text" placeholder="Enter name to search...">
+                    <button><i class="fas fa-search"></i></button>
+
+                </div>
+
+                <div class="users-list">
+
+                </div>
+
+            </section>
+        </div>
+
+    </body>
+</html>
