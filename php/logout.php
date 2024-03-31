66be429694e74cf1a2acd2d87488599c85015137
nandin <>
2024-03-31 12:20:17 +0800

    Fix the issue in logout.php

--- php/logout.php
+++ php/logout.php
@@ -1,25 +1,29 @@
-<?php 
+<?php
 
     session_start();
-    if(isset($_SESSION['unique_id'])){
+
+    if (isset($_SESSION['unique_id'])) {
         include_once "config.php";
 
-        $logout_id = mysqli_real_escape_string($conn, $_GET['logout_id']);
-
-        if(isset($logout_id)){
+        if (isset($_GET['logout_id'])) {
+            $logout_id = mysqli_real_escape_string($conn, $_GET['logout_id']);
             $status = "Offline now";
 
-            $sql = mysqli_query($conn, "UPDATE users SET status = '{$status}' WHERE unique_id = {$_GET['logout_id']} ");
+            $sql = mysqli_query($conn, "UPDATE users SET status = '{$status}' WHERE unique_id = '{$logout_id}'");
 
-            if($sql){
+            if ($sql) {
                 session_unset();
                 session_destroy();
                 header("location: ../login.php");
             }
-        }else {
-            header("location: ../users.php");
+        } else {
+            session_unset();
+            session_destroy();
+            header("location: ../login.php");
         }
-    }else {
+    } else {
+        session_unset();
+        session_destroy();
         header("location: ../login.php");
     }
 
