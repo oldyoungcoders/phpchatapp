b562d4394dedd499dc83d86a26f3b71d80087825
nandin <>
2024-04-08 13:41:27 +0800

    9-r dasgal duusav

--- js/chat.js
+++ js/chat.js
@@ -17,6 +17,7 @@ inputField.onkeyup = () => {
 sendBtn.onclick = () => {
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "php/insert-chat.php", true);
+
     xhr.onload = () => {
         if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
             inputField.value = "";
@@ -34,6 +35,27 @@ chatBox.onmouseenter = () => {
 chatBox.onmouseleave = () => {
     chatBox.classList.remove("active");
 };
+
+setInterval(() => {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "php/get-chat.php", true);
+
+    xhr.onload = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if(xhr.status === 200){
+                let data = xhr.response; 
+                chatBox.innerHTML = data;
+                if(!chatBox.classList.contains("active")){
+                    scrollToBottom();
+                }
+            }
+        }
+    };
+
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send("incoming_id="+incoming_id);
+
+}, 500);
 
 function scrollToBottom() {
     chatBox.scrollTop = chatBox.scrollHeight;
